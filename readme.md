bac2bf1eda783762beb6eed2acb419597902a66e
Enrico Zandomeni Borba <enricozb@gmail.com>
2023-03-25 08:54:01 +0100

    basic args

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Omnizip - Multiformat (Un)Compression
+
+## Usages
+```sh
+# compress files or directories
+zip -o <destination file> [directory or file...]
+
+# extract a file to a directory
+zip -x <file to decompress> [directory]
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about)]
+struct Args {
+  #[clap(short, long, conflicts_with = "extract")]
+  output: Option<String>,
+  #[clap(short, long, conflicts_with = "output")]
+  extract: Option<String>,
+  #[clap(required = true)]
+  files: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+  let _ = Args::parse();
 }
